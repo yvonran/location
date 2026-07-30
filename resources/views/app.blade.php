a91faa0a001300158e9ec9d3aa77275a56492c60
Yvon Sahaza Group <yvon.randrianavony@sahaza-group.com>
2026-07-30 15:27:23 +0300

    Add web app manifest and update favicon links
    - Created a new web app manifest file (site.webmanifest) with app details and icons.
    - Added 192x192 and 512x512 PNG icons for the web app.
    - Updated favicon links in app.blade.php to include new icon sizes and types.
    - Included a meta tag for the Apple mobile web app title.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,10 +30,12 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MS Location" />
+        <link rel="manifest" href="/site.webmanifest" />
         @fonts
 
         @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
